af812bb1cdc9b98904e79e12511984a3cc6a5ceb
Josefine <70942956+josefineweinefalk@users.noreply.github.com>
2021-11-14 14:25:33 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,42 @@
-public class Main {
+package sankaskepp;
 
-    public static void main(String[]arg){
-        GameBoard gameBoard = new GameBoard();
-        gameBoard.printBoard();
-    }
-}
+import java.util.Scanner;
 
-}
+public class P extends RunGame {
+	private static Scanner scan = new Scanner(System.in);
+	private static boolean gameEnded;
+	private static boolean playerWon;
+	private static double playerShots;
+	private static double computerShots;
+	private static String playerName;
+	protected static long p1Time;
+
+	public static void main(String[] args) {
+		fillGrid();
+		createList();
+		playerPlaceShips();
+		compPlaceShips();
+		System.out.println();
+
+		while (!gameEnded) {
+			playerTurn();
+			if (allShipsSinked()) {
+				System.out.println("Du har sänkt alla spelaren skepp!");
+				playerWon = true;
+				break;
+			}
+			computerTurn();
+			if (allShipsSinked()) {
+				System.out.println("Spelare2 sänkte alla dina skepp!");
+				break;
+			}
+		}
+		if (playerWon) {
+			System.out.println("Du vann över spelaren!");
+			System.out.println("Du slog datorn på " + Math.round(playerShots) + " rundor!");
+			System.out.println();
+		} else {
+			System.out.println();
+			System.out.println("Spelaren vann över dig på " + Math.round(computerShots) + " rundor!");
+		}
+	}
